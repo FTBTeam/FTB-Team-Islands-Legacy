a41e03ab9dd795203671932fbea7c789f4523b5d
LatvianModder <latvianmodder@gmail.com>
2019-03-31 00:43:32 +0200

    Fixed click in /teamislands list

--- src/main/java/com/feed_the_beast/mods/teamislands/command/CommandListIslands.java
+++ src/main/java/com/feed_the_beast/mods/teamislands/command/CommandListIslands.java
@@ -1,12 +1,8 @@
 package com.feed_the_beast.mods.teamislands.command;
 
-import com.feed_the_beast.ftblib.lib.command.CommandUtils;
-import com.feed_the_beast.ftblib.lib.data.ForgePlayer;
 import com.feed_the_beast.ftblib.lib.data.ForgeTeam;
 import com.feed_the_beast.ftblib.lib.data.Universe;
-import com.feed_the_beast.mods.teamislands.data.TeamIslandsUniverseData;
 import net.minecraft.command.CommandBase;
-import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.ITextComponent;
@@ -31,20 +27,16 @@ public class CommandListIslands extends CommandBase
 	}
 
 	@Override
-	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
+	public void execute(MinecraftServer server, ICommandSender sender, String[] args)
 	{
 		for (ForgeTeam team : Universe.get().getTeams())
 		{
-			ITextComponent component = new TextComponentString("- ").appendSibling(team.getTitle().createCopy());
+			ITextComponent title = team.getTitle().createCopy();
+			title.getStyle().setClickEvent(null);
+			title.getStyle().setHoverEvent(null);
+			ITextComponent component = new TextComponentString("- ").appendSibling(title);
 			component.getStyle().setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/teamislands island " + team.toString()));
 			sender.sendMessage(component);
-		}
-
-		ForgePlayer player = CommandUtils.getForgePlayer(sender, args[0]);
-
-		if (player.isOnline() && player.hasTeam())
-		{
-			TeamIslandsUniverseData.INSTANCE.getIsland(player.team).teleport(player.getPlayer());
 		}
 	}
 }
